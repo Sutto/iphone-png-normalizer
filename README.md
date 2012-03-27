81c92ad729d6606c1a41b36ea187894a020c8a6a
Darcy Laycock <sutto@sutto.net>
2012-03-27 20:42:17 +0800

    Update readme, massive refactoring of code

---
+++ README.md
@@ -0,0 +1,17 @@
+# Original Notes
+
+The original repository is from: [https://github.com/swcai/iphone-png-normalizer](https://github.com/swcai/iphone-png-normalizer).
+
+To quote,
+
+> The PNG file format inside iPhone application is slightly different from
+> normal PNG files. So when you get the png images from iphone app package,
+> you should make some conversion.
+
+> I wrote this handy Ruby script to do it for me. Let me know if you think
+> it is useful.
+
+## Fork Notes
+
+I found it super useful! I added a slight layer to make it useful (as a replacement for pngcrush w/
+minimal options) on a linux system. Huzzah for Open Source!
--- iphonepng.rb
+++ iphonepng.rb
@@ -15,114 +15,191 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-=begin
-  TODO unit test
-=end
 
 require "zlib"
+require "optparse"
 
-def decompress(data)
-  # make zlib not check the header
-  zstream = Zlib::Inflate.new(-Zlib::MAX_WBITS)
-  buf = zstream.inflate(data)
-  zstream.finish
-  zstream.close
-  buf
-end
+class ApplePngNormalizer
 
-def compress(data)
-  Zlib::Deflate.deflate(data)
-end
+  class Error < StandardError; end
+  class CorruptPNG < Error; end
 
-PNG_HEADER = "\x89PNG\r\n\x1a\n"
+  PNG_HEADER = "\x89PNG\r\n\x1a\n"
 
-def normalize(oldPNG)
-  if oldPNG[0, 8] != PNG_HEADER then
-    puts "Corrupted PNG file"
-    return nil
+  Section = Struct.new(:type, :length, :data, :crc, :width, :height)
+
+  def normalize(source_data)
+    source_data.force_encoding PNG_HEADER.encoding if source_data.respond_to?(:force_encoding)
+    destination_data = source_data[0, PNG_HEADER.length].dup
+    raise CorruptPNG unless destination_data == PNG_HEADER
+    sections = extract_sections source_data, destination_data.length
+    reconstruct_sections destination_data, sections
+    destination_data
   end
-  
-  newPNG = String.new(oldPNG[0, 8])
-  pos = 8
-  sections = []
 
-  while pos < oldPNG.length
-    
-    # use "N" instead of "L", using network endian not native endian
-    length = oldPNG[pos, 4].unpack('N')[0]
-    type = oldPNG[pos+4, 4]
-    data = oldPNG[pos+8, length]
-    crc = oldPNG[pos+8+length, 4].unpack('N')[0]
-    pos += length + 12
-    
-    next if type == "CgBI"
-    
-    if type == "IHDR" then
-      width = data[0, 4].unpack("N")[0]
-      height = data[4, 4].unpack("N")[0]
+
+  # Given a single set of data, will return the normalized png
+  # and return the normalized data.
+  def self.normalize(data)
+    instance = new
+    instance.normalize data
+  end
+
+  # Given a source file and a destination file, will attempt
+  # to save the reverted version of the file from source path
+  # at destination path.
+  def self.normalize_file(source_path, destination_path)
+    raise ArgumentError, "#{source_path} does not exist." unless File.exist?(source_path)
+    original = File.open(source_path, 'rb') { |f| f.read }
+    normalized = self.normalize File.read(source_path)
+    File.open(destination_path, 'wb') do |file|
+      file.write normalized
+    end if normalized
+  end
+
+  # Searches recursively under the given path and normalises any found
+  # pngs. This is the default mode and invoked on the command line
+  # without a --help argument.
+  def self.normalize_under(path)
+    Dir[File.join(path, "**/*.png")].each do |png|
+      # Skip already normalized pngs.
+      next if png =~ /_norm\.png$/
+      # and process any that aren't.
+      normalize_file png, png.gsub(/\.png$/, '_norm.png')
     end
+  end
 
-    if type == 'IDAT' && sections.size > 0 && sections.last.first == 'IDAT'
-      # Append to the previous IDAT
-      sections.last[1] += length
-      sections.last[2] += data
+  def self.parse_options!(source = ARGV)
+    options[:quiet] = source.include? "-q"
+    options[:help]  = source.include? "--help"
+    source.reject! { |v| v[0, 1] == "-" }
+    options[:input]     = ARGV.shift
+    options[:output]    = ARGV.shift
+    options[:directory] = (options[:input] && File.directory?(options[:input]))
+    options[:help]      = !(options[:directory] || (options[:input] && options[:output]))
+  end
+
+  def self.options
+    @options ||= {}
+  end
+
+  def self.run
+    parse_options!
+    if options[:help]
+      STDERR.puts "Usage: #{$0} [-q] input-file output-file"
+      STDERR.puts "or...  #{$0} [-q] input-directory"
+      exit 1
+    elsif options[:directory]
+      normalize_under options[:input]
     else
-      sections << [type, length, data, crc, width, height]
+      normalize_file options[:input], options[:output]
     end
-
-    break if type == "IEND"
-    
   end
 
-  sections.each do |(type, length, data, crc, width, height)|
+  private
 
-    if type == "IDAT" then
+  def extract_sections(source_data, initial_offset)
+    pos      = initial_offset
+    sections = []
 
-      bufSize = width * height * 4 + height
-      data = decompress(data[0, bufSize])
-
-      # duplicate the content of old data at first to avoid creating too many string objects
-      newdata = String.new(data)
-      pos = 0
-
-      for y in 0...height
-        newdata[pos] = data[pos, 1]
-        pos += 1
-        for x in 0...width
-          newdata[pos+0] = data[pos+2, 1]
-          newdata[pos+1] = data[pos+1, 1]
-          newdata[pos+2] = data[pos+0, 1]
-          newdata[pos+3] = data[pos+3, 1]
-          pos += 4
-        end
+    while pos < source_data.length
+      # use "N" instead of "L", using network endian not native endian
+      length = source_data[pos, 4].unpack('N')[0]
+      type   = source_data[pos + 4, 4]
+      data   = source_data[pos + 8, length]
+      crc    = source_data[pos + 8 + length, 4].unpack('N')[0]
+      pos   += (length + 12)
+      
+      # Ignore the CgBI chunk
+      next if type == "CgBI"
+      
+      if type == "IHDR"
+        width  = data[0, 4].unpack("N")[0]
+        height = data[4, 4].unpack("N")[0]
       end
 
-      data = compress(newdata)
-      length = data.length
-      crc = Zlib::crc32(type)
-      crc = Zlib::crc32(data, crc)
-      crc = (crc + 0x100000000) % 0x100000000
+      # When we encounter multiple sequential IDAT sections, concatenate them together.
+      if type == 'IDAT' && sections.size > 0 && sections.last.first == 'IDAT'
+        # Append to the previous IDAT
+        sections.last.length += length
+        sections.last.data += data
+      else
+        sections << Section.new(type, length, data, crc, width, height)
+      end
+
+      # Stop once we encounter the IEND section.
+      break if type == "IEND"
+      
     end
 
-    newPNG += [length].pack("N") + type + (data || '') + [crc].pack("N")
-
+    sections
   end
 
-  newPNG
-end
+  def reconstruct_sections(buffer, sections)
 
+    # (type, length, data, crc, width, height)
 
-def normalize_png(filename)
-  puts "#{filename}"
-  File.open(filename, 'rb') do |file|
-    newPNG = normalize(file.read())
-    if newPNG != nil then
-      newFilename = File.basename(filename, ".*") + "_norm" + File.extname(filename)
-      File.new(newFilename, 'wb').write(newPNG)
+    sections.each do |section|
+
+      crc    = section.crc
+      data   = section.data
+      length = section.length
+
+      if section.type == "IDAT"
+
+        width, height = section.width, section.height
+
+        buffer_size = width * height * 4 + height
+        data        = decompress section.data[0, buffer_size]
+
+        # duplicate the content of old data at first to avoid creating too many string objects
+        chunk_data = data.dup
+        pos     = 0
+
+        # Restructure each section to fit inside the data correct
+        (0...height).each do |y|
+          chunk_data[pos] = data[pos, 1]
+          pos += 1
+          (0...width).each do |y|
+            chunk_data[pos + 0] = data[pos + 2, 1]
+            chunk_data[pos + 1] = data[pos + 1, 1]
+            chunk_data[pos + 2] = data[pos + 0, 1]
+            chunk_data[pos + 3] = data[pos + 3, 1]
+            pos += 4
+          end
+        end
+
+        data   = compress chunk_data
+        length = data.length
+
+        # Recalculate the crc.
+        crc = Zlib.crc32 section.type
+        crc = Zlib.crc32 data, crc
+      end
+
+      # Repack the section into the image.
+      buffer << [length].pack("N")
+      buffer << section.type
+      buffer << (data || '')
+      buffer << [crc].pack("N")
     end
   end
+
+
+  def decompress(data)
+    # Makes it such that zlib will not check the header before
+    # inflating.
+    zstream = Zlib::Inflate.new -Zlib::MAX_WBITS
+    buf     = zstream.inflate(data)
+    zstream.finish
+    zstream.close
+    buf
+  end
+
+  def compress(data)
+    Zlib::Deflate.deflate data
+  end
+
 end
 
-# ARGV[0] = '.' 
-ARGV.each {|a| Dir.glob("#{a}/*.png").each {|file| normalize_png(file)}}
-puts "Done."
+ApplePngNormalizer.run
